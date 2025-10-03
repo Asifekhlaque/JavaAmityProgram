87a32d6d961f2b8b5e1d0f84ee1bb8a7145078a0
user.name like "Asif Ekhlaque <asifekhlaque33@gmail.com>
2025-10-03 15:31:37 +0530

    patten wala question

--- pattent.java
+++ pattent.java
@@ -1,5 +1,14 @@
 public class pattent{
     public static void main(String[] args) {
+        // Write a java program to print the following pattern using array
+        int arr[][]={{1},{1,2},{1,2,3},{1,2,3,4}};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+        // Write a java program to print the following pattern using for loop
         for(int i=1;i<=5;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(j);
